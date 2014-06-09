fa05a6ddd7c25be5dbf3a5ee14499f42a8cec19d
Radim Kubacki <radim@kubacki.cz>
2014-06-09 12:05:39 +0200

    Use init script to inject plugin into any Gradle build.

--- dependencyClient/src/main/java/org/gradle/sample/dependencies/lib/ModelProvider.java
+++ dependencyClient/src/main/java/org/gradle/sample/dependencies/lib/ModelProvider.java
@@ -1,10 +1,20 @@
 package org.gradle.sample.dependencies.lib;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+import com.google.common.io.Resources;
 import org.gradle.sample.plugin.dependencies.ConfigurationDependenciesModel;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class ModelProvider {
 
@@ -16,6 +26,7 @@ public class ModelProvider {
 
     private String path;
     private ProjectConnection connection;
+    private String initScriptPath;
 
     private ModelProvider() {
     }
@@ -25,16 +36,34 @@ public class ModelProvider {
         connection = null;
     }
 
-    private synchronized void initConnection() {
+    private synchronized void initConnection() throws IOException, URISyntaxException {
         GradleConnector connector = GradleConnector.newConnector();
         connector.forProjectDirectory(new File(path));
 
         connection = connector.connect();
+
+        Path pluginJarPath = Paths.get(ConfigurationDependenciesModel.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+        String pluginDirPath = pluginJarPath.toFile().getParentFile().getAbsolutePath();
+        String script = Resources.toString(ModelProvider.class.getResource("init-dependencies.gradle"), Charsets.UTF_8);
+        script = script.replace("<PLUGIN_DIR>", pluginDirPath);
+
+        Path initScript = Files.createTempFile("init-dependencies", ".gradle");
+        initScriptPath = initScript.toFile().getAbsolutePath();
+        Files.write(initScript, script.getBytes(Charsets.UTF_8));
+        initScript.toFile().deleteOnExit();
     }
+
     public ConfigurationDependenciesModel getConfigurationDependencies() {
-        initConnection();
+        try {
+            initConnection();
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
         // Load the custom model for the project
-        ConfigurationDependenciesModel model = connection.getModel(ConfigurationDependenciesModel.class);
+        ConfigurationDependenciesModel model = connection.model(ConfigurationDependenciesModel.class)
+                .withArguments("--init-script", initScriptPath)
+                .get();
         return model;
     }
 }
